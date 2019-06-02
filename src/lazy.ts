8b210c26e8aa1d701849fb767d6241fa2ac025da
Alex Burner <alex.burner@gmail.com>
2019-06-01 21:18:50 -0700

    Remove config test cruft & run build

--- src/lazy.ts
+++
@@ -1,3 +0,0 @@
-console.log('lazy root')
-
-export const test = () => console.log('lazy test')
--- src/main.ts
+++ src/main.ts
@@ -16,12 +16,6 @@ const now = new Date()
 const dates = makeYearDates(now.getFullYear())
 
 function makeTableSet(interpolateColor: InterpolateColor): HTMLElement {
-  if (true) {
-    function inner() {
-      console.log('inner')
-    }
-    inner()
-  }
   const el = document.createElement('div')
   el.appendChild(makeDaysTable(now, dates, interpolateColor))
   el.appendChild(makeMonthsTable(now, dates, interpolateColor))
@@ -37,11 +31,3 @@ root.appendChild(makeTableSet(interpolateHcl))
 // root.appendChild(makeTableSet(interpolateHsl))
 // root.appendChild(document.createElement('hr'))
 // root.appendChild(makeTableSet(interpolateRgb))
-
-setTimeout(async () => {
-  const foo = await import('./lazy')
-  foo.test()
-}, 1000)
-
-const test = new Set(['test!'])
-console.log(test)
