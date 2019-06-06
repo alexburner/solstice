517f7bffa431424ecde539efb50a1ea513ab0e00
Alex Burner <alex.burner@gmail.com>
2019-06-05 23:12:00 -0700

    Update label text

--- src/util/labels.ts
+++ src/util/labels.ts
@@ -4,9 +4,9 @@ import { getSeason } from './seasons'
 
 type DateStr = (date: Date) => string
 
-export const dayStr: DateStr = date => format(date, 'Do')
-export const monthStr: DateStr = date => format(date, 'M|MMM')
-export const quarterStr: DateStr = date => format(date, 'YY-[Q]Q')
+export const dayStr: DateStr = date => format(date, 'ddd')
+export const monthStr: DateStr = date => format(date, 'MMM D')
+export const quarterStr: DateStr = date => format(date, "[Q]Q'19")
 export const seasonStr: DateStr = date => {
   const season = getSeason(date.getFullYear(), date.getMonth(), date.getDate())
   return season.name
